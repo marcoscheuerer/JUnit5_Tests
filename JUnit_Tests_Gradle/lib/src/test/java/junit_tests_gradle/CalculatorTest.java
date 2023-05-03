87e553641e8a40a1dec5d57907b19b6f2c53a5a2
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-05-04 00:45:00 +0200

    Assertions and Test Structure

--- JUnit_Tests_Gradle/lib/src/test/java/junit_tests_gradle/CalculatorTest.java
+++ JUnit_Tests_Gradle/lib/src/test/java/junit_tests_gradle/CalculatorTest.java
@@ -1,12 +1,15 @@
 package junit_tests_gradle;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.DisplayName;
 import java.lang.IllegalArgumentException;
 
 public class CalculatorTest {
@@ -18,19 +21,29 @@ public class CalculatorTest {
 		calculator = new Calculator();
 	}
 	
-	@Test
-	void positiveSum_twoPositiveInteger_returnSum() {
-		int sum = calculator.positiveSum(3, 5);
-		assertTrue(sum > 0);
-		assertEquals(8, sum);
-		assertFalse(sum <= 0);
-		assertNotNull(sum);
+	@Nested
+	@DisplayName("all successful cases for calculator class")
+	class SuccessCases {
+		@Test
+		void positiveSum_twoPositiveInteger_returnSum() {
+			int sum = calculator.positiveSum(3, 5);
+			assertAll(
+			  () -> assertTrue(sum > 0),
+			  () -> assertEquals(8, sum),
+			  () -> assertFalse(sum <= 0),
+			  () -> assertNotNull(sum)
+			);
+		}
 	}
 	
-	@Test
-	void positiveSum_negativeInteger_throwsException() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			calculator.positiveSum(-4, 5);
-		});
+	@Nested
+	@DisplayName("all failure cases for calculator class")
+	class FailureCases {
+		@Test
+		void positiveSum_negativeInteger_throwsException() {
+			assertThrows(IllegalArgumentException.class, () -> {
+				calculator.positiveSum(-4, 5);
+			});
+		}
 	}
 }
