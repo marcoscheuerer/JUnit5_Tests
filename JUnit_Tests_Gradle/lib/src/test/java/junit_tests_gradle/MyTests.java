17c5d6146da3280eb0ee41f6436787fd978284b9
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-05-03 16:11:04 +0200

    JUnit fundamental tests in gradle

---
+++ JUnit_Tests_Gradle/lib/src/test/java/junit_tests_gradle/MyTests.java
@@ -0,0 +1,43 @@
+package junit_tests_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class MyTests {
+
+	@BeforeAll
+	void runBeforeAll() {
+		System.out.println("run once before all tests");
+	}
+	
+	@BeforeEach
+	void runBeforeEach() {
+		System.out.println("before each test method");
+	}
+	
+	@Test
+	void firstTest() {
+		System.out.println("hello world in firstTest() method");
+	}
+	
+	@Test
+	void secondTest() {
+		System.out.println("hello world in secondTest() method");
+	}
+	
+	@AfterEach
+	void runAfterEach() {
+		System.out.println("after each test method");
+	}
+	
+	@AfterAll
+	void runAfterAll() {
+		System.out.println("run once after all tests");
+	}
+	
+}
